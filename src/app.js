94f6669fd25f2fa7577dc8438e6cdb67ed31eef9
Henry Xiang <henryxiang@gmail.com>
2017-03-15 15:39:11 -0700

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import './app.css';
 import $ from 'jquery';
 
-$('#app').append('<a href="#" id="showCats">Show Cats</a><br/><ul id="cats"></ul>');
+$('#app').append('<a href="#" id="showCats">Show Cats</a><br/><div id="viewPort"/>');
 
 $('#showCats').click(() => {
   System
     .import('./cats.js')
     .then((module) => {
       console.log("cats:", module.default);
+      $('#viewPort').html('<ul id="cats"></ul>');
       for (let cat of module.default) {
         $('#cats').append(`<li>${cat}</li>`);
       }
     })
-
 })
